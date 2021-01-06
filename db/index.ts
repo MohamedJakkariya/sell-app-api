8f5dc8e107b2a1f3002f2c80e042e516ef3bdd5d
MohamedJakkariya <mohamed.jakkariya@fleksa.com>
2021-01-06 21:25:36 +0530

    updated ts for db configurations

--- db/index.ts
+++ db/index.ts
@@ -6,7 +6,7 @@ const Logger = require('js-logger');
  * @desc :   Get all data from the given tables
  * @return : Promise
  */
-exports.poolConnect = pool => {
+exports.poolConnect = (pool: { getConnection: (arg0: (err: any, connection: any) => void) => void }) => {
   return new Promise((resolve, reject) => {
     pool.getConnection((err, connection) => {
       if (err) return reject(err); // not connected!
@@ -21,7 +21,10 @@ exports.poolConnect = pool => {
  * @return : <Promise>
  * @desc :   Get all data from the given tables
  */
-exports.getAll = (connection, options) => {
+exports.getAll = (
+  connection: { query: (arg0: string, arg1: (err: any, results: any) => void) => void },
+  options: { projections: any; table_names: any }
+) => {
   return new Promise((resolve, reject) => {
     connection.query(`SELECT ${options.projections} FROM ${options.table_names}`, (err, results) => {
       if (err) return reject(err);
@@ -36,7 +39,10 @@ exports.getAll = (connection, options) => {
  * @return : <Promise>
  * @desc :   Get all data from the given tables
  */
-exports.getOne = (connection, options) => {
+exports.getOne = (
+  connection: { query: (arg0: string, arg1: any, arg2: (err: any, result: any) => void) => void },
+  options: { projection: any; table_name: any; condition: any; values: any }
+) => {
   return new Promise((resolve, reject) => {
     connection.query(
       `SELECT ${options.projection} FROM ${options.table_name} WHERE ${options.condition}`,
@@ -54,7 +60,10 @@ exports.getOne = (connection, options) => {
  * @return : <Promise>
  * @desc :   Get all data from the given tables
  */
-exports.insertOne = (connection, options) => {
+exports.insertOne = (
+  connection: { query: (arg0: string, arg1: any, arg2: (err: any, results: any) => void) => void },
+  options: { table_name: any; data: any }
+) => {
   return new Promise((resolve, reject) => {
     connection.query(`INSERT INTO ${options.table_name} SET ?`, options.data, (err, results) => {
       if (err) return reject(err);
@@ -70,7 +79,10 @@ exports.insertOne = (connection, options) => {
  * @return : <Promise>
  * @desc :   Get all data from the given tables
  */
-exports.deleteOne = (connection, options) => {
+exports.deleteOne = (
+  connection: { query: (arg0: string, arg1: any, arg2: (err: any, result: any) => void) => void },
+  options: { table_name: any; condition: any; value: any }
+) => {
   return new Promise((resolve, reject) => {
     connection.query(`DELETE FROM ${options.table_name} WHERE ${options.condition}`, options.value, (err, result) => {
       if (err) return reject(err);
@@ -86,7 +98,10 @@ exports.deleteOne = (connection, options) => {
  * @return : <Promise>
  * @desc :   Get all data from the given tables
  */
-exports.getMulti = (connection, options) => {
+exports.getMulti = (
+  connection: { query: (arg0: string, arg1: any, arg2: (err: any, results: any) => void) => void },
+  options: { projections: any; table_names: any; conditions: any; values: any }
+) => {
   return new Promise((resolve, reject) => {
     connection.query(
       `select ${options.projections}  from ${options.table_names} where ${options.conditions} 
@@ -106,7 +121,10 @@ exports.getMulti = (connection, options) => {
  * @return : <Promise>
  * @desc :   Get all data from the given tables
  */
-exports.updateOne = (connection, options) => {
+exports.updateOne = (
+  connection: { query: (arg0: string, arg1: any[], arg2: (err: any, results: any) => void) => void },
+  options: { table_name: any; updating_fields: any; key: any; updating_values: any; value: any }
+) => {
   return new Promise((resolve, reject) => {
     connection.query(
       `UPDATE ${options.table_name} SET ${options.updating_fields} WHERE ${options.key} = ?`,
@@ -126,7 +144,10 @@ exports.updateOne = (connection, options) => {
  * @return : <Promise>
  * @desc :   Get all data from the given tables
  */
-exports.foreignKeyMode = (connection, mode) => {
+exports.foreignKeyMode = (
+  connection: { query: (arg0: string, arg1: any, arg2: (err: any) => void) => void },
+  mode: number
+) => {
   return new Promise((resolve, reject) => {
     connection.query(`SET FOREIGN_KEY_CHECKS = ?`, mode, err => {
       if (err) return reject(err);
@@ -142,7 +163,7 @@ exports.foreignKeyMode = (connection, mode) => {
  * @return : Error<stack>
  * @desc :   Return err depends on call back
  */
-exports.rollback = (connection, err) =>
+exports.rollback = (connection: { rollback: (arg0: () => never) => any }, err: any) =>
   connection.rollback(() => {
     throw err;
   });
@@ -151,7 +172,7 @@ exports.rollback = (connection, err) =>
  * @desc :   Complete the transaction
  * @return : boolean or err stack
  */
-exports.commit = connection => {
+exports.commit = (connection: { commit: (arg0: (err: any) => any) => void; rollback: (arg0: () => never) => any }) => {
   connection.commit(err => {
     if (err)
       return connection.rollback(() => {
@@ -168,13 +189,16 @@ exports.commit = connection => {
  * @return : <Promise>
  * @desc :   Insert data into multiple tables
  */
-exports.insertIntoMultiTables = (connection, options) => {
+exports.insertIntoMultiTables = (
+  connection: { query: (arg0: string, arg1: any[], arg2: (err: any, results: any) => void) => void },
+  options: any[]
+) => {
   return new Promise((resolve, reject) => {
     const baseQ = `INSERT INTO ? SET ? ; `;
 
     let genQ = baseQ.repeat(options.length);
 
-    const data = [];
+    const data: any[] = [];
 
     // make our query with data array
     options.forEach(v => {
@@ -197,7 +221,10 @@ exports.insertIntoMultiTables = (connection, options) => {
  * @return : <Promise>
  * @desc :   Insert multiple data into same tables
  */
-exports.insertIntoMultiData = (connection, options) => {
+exports.insertIntoMultiData = (
+  connection: { query: (arg0: string, arg1: any, arg2: (err: any, results: any) => void) => void },
+  options: { data: string | any[]; table_name: any }
+) => {
   return new Promise((resolve, reject) => {
     const baseQ = `INSERT INTO SET ? ; `;
 
@@ -209,7 +236,7 @@ exports.insertIntoMultiData = (connection, options) => {
     connection.query(`${genQ}`, options.data, (err, results) => {
       if (err) return reject(err);
 
-      results.forEach(i => Logger.log(`Inserted id ${i.insertId}!`));
+      results.forEach((i: { insertId: any }) => Logger.log(`Inserted id ${i.insertId}!`));
       return resolve(results);
     });
   });
